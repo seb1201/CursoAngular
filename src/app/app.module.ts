abc8bebf5486b26ed021f7d6ff16905046e28e48
Sebastian Vargas <sebastian.vargas@biaani.com>
2019-12-03 14:06:41 -0600

    Template sin rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { CatalogoComponent } from './pages/catalogo/catalogo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CatalogoComponent
   ],
   imports: [
     BrowserModule,
