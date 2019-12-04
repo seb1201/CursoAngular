aac2c2baa77b2191e293e0f1610b3aebd2d55978
Sebastian Vargas <sebastian.vargas@biaani.com>
2019-12-04 17:51:16 -0600

    actuliacion s_s

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
 
-const routes: Routes = [];
+const routesapp: Routes = [
+  {path: 'home', component: CatalogoComponent },
+  {path: 'about', component: AboutComponent },
+  {path: 'item', component: ItemComponent },
+  {path: 'catalogo', component: CatalogoComponent},
+  {path: '***', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routesapp, { useHash: true })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { CatalogoComponent } from './pages/catalogo/catalogo.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    CatalogoComponent
+    CatalogoComponent,
+    AboutComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
