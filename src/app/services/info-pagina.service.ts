eb3798bb0c3f1e6ab42253f3bed7df51f8f1d899
Sebastian Vargas <sebastian.vargas@biaani.com>
2019-12-05 18:52:30 -0600

    servicio info con firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,8 +9,15 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient) {
+
+     this.cargarInfo();
+     this.cargasEquipo();
+   }
+
+   private cargarInfo() {
 
     console.log('Service infoPagina successful');
     this.http.get('assets/data/data-pagina.json')
@@ -18,9 +25,23 @@ export class InfoPaginaService {
 
      this.cargada = true;
      this.info = resp;
+
+
+    });
+
+   }
+
+   private cargasEquipo() {
+
+    console.log('Service infoPagina successful');
+    this.http.get('https://sebas-store.firebaseio.com/miembros.json')
+    .subscribe( (resp: any[]) => {
+
+     this.equipo = resp;
      console.log(resp);
 
 
     });
+
    }
 }
